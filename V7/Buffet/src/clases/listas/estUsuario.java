46d658147a0b4e74b431043be7394dd4eee256a8
sergioigna <sergio.posso@pragma.com.co>
2014-05-26 23:22:36 -0500

    commit seven

---
+++ V7/Buffet/src/clases/listas/estUsuario.java
@@ -0,0 +1,20 @@
+package clases.listas;
+
+public class estUsuario {
+    
+    public estUsuario liga;
+    public String usuario, contrasena, nombre, apellido, email, departamento, perfil, estado, fecha;
+    
+    public estUsuario (){
+        liga = null;
+        usuario = "";
+        contrasena= "";
+        nombre = "";
+        apellido = "";
+        email = "";
+        //departamento= "";
+        perfil= "";
+        estado= "";
+        fecha = "";
+    }
+}
