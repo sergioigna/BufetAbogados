48e48a6d46c7b2e4e0be613f13b2426b23a758d3
sergioigna <sergio.posso@pragma.com.co>
2014-05-26 23:09:54 -0500

    commit four

---
+++ V4/Buffet/src/clases/listas/estArticulo.java
@@ -0,0 +1,18 @@
+package clases.listas;
+
+public class estArticulo {
+    
+    public estArticulo liga;
+    public String lugar, fecha, horaI, horaF, titulo, descripcion , estado;
+    
+    public estArticulo (){
+        liga = null;
+        fecha = "";
+        horaI= "";
+        horaF = "";
+        titulo = "";
+        descripcion = "";
+        lugar= "";
+        estado="";
+    }
+}
---
+++ V4/Buffet/src/clases/listas/estReparacion.java
@@ -0,0 +1,19 @@
+package clases.listas;
+
+public class estReparacion {
+    
+    public estReparacion liga;
+    public String usuario, apellido, DocSolicitante, codigoE, lugarE, codigoA, hora, fecha;
+    
+    public estReparacion (){
+        liga = null;
+        usuario = "";
+        apellido= "";
+        DocSolicitante = "";
+        codigoE = "";
+        lugarE = "";
+        codigoA= "";
+        hora = "";
+        fecha = "";
+    }
+}
