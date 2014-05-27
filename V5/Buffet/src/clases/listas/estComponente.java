5b7dd6dbf634a6430eb21f0f6a0f557b21b815e0
sergioigna <sergio.posso@pragma.com.co>
2014-05-26 23:14:08 -0500

    commit five

---
+++ V5/Buffet/src/clases/listas/estComponente.java
@@ -0,0 +1,17 @@
+package clases.listas;
+
+public class estComponente {
+    
+    public estComponente liga;
+    public String codigo, nombre, proceso, tipoExp, descripcion, acceso;
+    
+    public estComponente (){
+        liga = null;
+        codigo = "";
+        proceso= "";
+        nombre = "";
+        tipoExp = "";
+        acceso = "";
+        descripcion= "";
+    }
+}
