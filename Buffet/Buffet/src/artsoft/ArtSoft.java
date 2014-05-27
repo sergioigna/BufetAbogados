7f5b434180660bd9242c7b1f44c8fb7605c256b0
sergioigna <sergio.posso@pragma.com.co>
2014-05-26 22:24:07 -0500

    Version uno

---
+++ Buffet/Buffet/src/artsoft/ArtSoft.java
@@ -0,0 +1,11 @@
+package artsoft;
+
+import formularios.FrmEntrada;
+
+public class ArtSoft {
+
+    public static void main(String[] args) {
+        FrmEntrada formulario = new FrmEntrada();
+        formulario.setVisible(true);
+    }
+}
